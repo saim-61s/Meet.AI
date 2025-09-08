2a4c53058412d501cba89b6dd41e71dbf512d609
Saim <samailahmad61@gmail.com>
2025-09-08 14:01:39 +0530

    07: dashboard navbar

--- meetai/src/app/(dashboard)/layout.tsx
+++ meetai/src/app/(dashboard)/layout.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { DashboardSideBar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
+
 
 interface Props {
     children: React.ReactNode;
@@ -9,7 +13,8 @@ const Layout = ({ children }: Props) => {
     return ( 
         <SidebarProvider>
             <DashboardSideBar />
-            <main className="flex flex-col h-screen">
+            <main className="flex flex-col h-screen w-screen bg-muted">
+                <DashboardNavbar />
                 {children}
             </main>
         </SidebarProvider>
