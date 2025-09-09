34ab4c2bceb32eae27150947c0684ef97f96f860
Saim <samailahmad61@gmail.com>
2025-09-10 01:50:33 +0530

    11: Agents Form

--- meetai/src/modules/agents/server/procedures.ts
+++ meetai/src/modules/agents/server/procedures.ts
@@ -1,9 +1,22 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getMany: baseProcedure.query(async () => {
+    //TODO:  Change `getOne` to use `protectedProcedure`
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+        const [existingAgent] = await db
+            .select()
+            .from(agents)
+            .where(eq(agents.id, input.id))
+
+        return existingAgent;
+    }),
+
+    getMany: protectedProcedure.query(async () => {
         const data = await db
             .select()
             .from(agents);
@@ -13,4 +26,17 @@ export const agentsRouter = createTRPCRouter({
 
         return data;
     }),
+    create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+        const [createdAgent] = await db
+            .insert(agents)
+            .values({
+                ...input,
+                userId: ctx.auth.user.id,
+            })
+            .returning();
+
+        return createdAgent;
+    })
 });
