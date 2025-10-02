08ef5696f3f55a3b0d5029527d6f03a34923916a
Saim <samailahmad61@gmail.com>
2025-10-02 14:11:18 +0530

    24: background-jobs

--- meetai/src/app/api/webhook/route.ts
+++ meetai/src/app/api/webhook/route.ts
@@ -1,9 +1,13 @@
 import { db } from "@/db";
 import { agents, meetings } from "@/db/schema";
 import { streamVideo } from "@/lib/stream-video";
-import type { CallSessionParticipantLeftEvent, CallSessionStartedEvent } from "@stream-io/video-react-sdk";
+import type { CallEndedEvent, CallRecordingReadyEvent, CallSessionParticipantLeftEvent, CallSessionStartedEvent, CallTranscriptionReadyEvent } from "@stream-io/video-react-sdk";
 import { and, eq, not } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+
+//Import the Google GenAi client -> make sure it's installed : npm install @google/genai
+import { CloudCog } from "lucide-react";
+import { inngest } from "@/inngest/client";
 
 function verifySignatureWithSDK(body: string, signature: string): boolean {
     return streamVideo.verifyWebhook(body, signature);
@@ -78,10 +82,11 @@ export async function POST(req: NextRequest) {
         }
 
         const call = streamVideo.video.call("default", meetingId);
+
         const realtimeClient = await streamVideo.video.connectOpenAi({
             call,
             openAiApiKey: process.env.OPENAI_API_KEY!,
-            agentUserId: existingAgent.id,
+            agentUserId:existingAgent.id,
         });
 
         realtimeClient.updateSession({
@@ -98,6 +103,55 @@ export async function POST(req: NextRequest) {
 
         const call = streamVideo.video.call("default", meetingId);
         await call.end();
+    } else if(eventType === "call.session_ended") {
+        const event = payload as CallEndedEvent;
+        const meetingId = event.call.custom?.meetingId;
+
+        if(!meetingId) {
+            return NextResponse.json({ error: "Missing meetingId" }, { status: 400 });
+        }
+
+        await db 
+            .update(meetings)
+            .set({
+                status: "processing",
+                endedAt: new Date(),
+            })
+            .where(and(eq(meetings.id, meetingId), eq(meetings.status, "active")));
+    } else if(eventType === "call.transcription_ready") {
+        const event = payload as CallTranscriptionReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+        const [updateMeeting] = await db 
+            .update(meetings)
+            .set({
+                transcriptUrl: event.call_transcription.url,
+            })
+            .where(eq(meetings.id, meetingId))
+            .returning();
+
+            //TODO: Call Ingest background job to summarize the transcript
+           if(!updateMeeting) {
+            return NextResponse.json({ error: "Meeting not found " }, { status: 400 });
+           }
+
+           await inngest.send({
+            name: "meetings/processing",
+            data: {
+                meetingId: updateMeeting.id,
+                transcriptUrl: updateMeeting.transcriptUrl,
+            }
+           })
+    } else if(eventType === "call.recording_ready") {
+        const event = payload as CallRecordingReadyEvent;
+        const meetingId = event.call_cid.split(":")[1];
+
+       await db 
+            .update(meetings)
+            .set({
+                recordingUrl: event.call_recording.url,
+            })
+            .where(and(eq(meetings.id, meetingId)));
     }
 
     return NextResponse.json({ status: "ok" });
